16b0d55c71b1f6e18400f86f6736a715469ff670
Nicola Brazzale <brazzalenicola@gmail.com>
2020-07-14 17:46:27 +0200

    update IRRCNN

--- src/IRRCNN.py
+++ src/IRRCNN.py
@@ -2,11 +2,11 @@ import tensorflow.keras as keras
 from tensorflow.keras.layers import Activation, BatchNormalization, Conv2D, Add
 from tensorflow.keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalAveragePooling2D
 from tensorflow.keras import backend as K
-
+import preprocessing
 
 # Implementation of the Improved Residual Recurrent Neural Network
 # Convolution 2D with batch normalization
-def conv_bn(x, nb_filter, num_row, num_col, padding='same', strides=(1, 1), use_bias=False):
+def conv_bn(X, nb_filter, num_row, num_col, padding='same', strides=(1, 1), use_bias=False):
     """
     Utility function to apply conv + BN.
     """
@@ -14,15 +14,14 @@ def conv_bn(x, nb_filter, num_row, num_col, padding='same', strides=(1, 1), use_
         channel_axis = 1
     else:
         channel_axis = -1
-
-    x = Conv2D(nb_filter, (num_row, num_col), strides=strides, padding=padding, use_bias=use_bias,
+    #num_row, num_col size of kernel
+    X = Conv2D(nb_filter, (num_row, num_col), strides=strides, padding=padding, use_bias=use_bias,
                kernel_regularizer=keras.regularizers.l2(1e-5),
                kernel_initializer=keras.initializers.VarianceScaling(scale=2.0, mode='fan_in', distribution='normal',
-                                                                     seed=None))(x)
-
-    x = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(x)
-    x = Activation('relu')(x)
-    return x
+                                                                     seed=None))(X)
+    X = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(X)
+    X = Activation('relu')(X)
+    return X
 
 
 # Recurrent convolutional layer
@@ -32,13 +31,13 @@ def RCL(input, kernel_size, filedepth):
     else:
         channel_axis = -1
 
-    conv1 = Conv2D(filters=filedepth, kernel_size=kernel_size, strides=(1, 1), padding='same',
+    X = Conv2D(filters=filedepth, kernel_size=kernel_size, strides=(1, 1), padding='same',
                    kernel_regularizer=keras.regularizers.l2(1e-4),
                    kernel_initializer=keras.initializers.VarianceScaling(scale=2.0, mode='fan_in',
                                                                          distribution='normal', seed=None))(input)
 
-    stack2 = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(conv1)
-    stack2 = Activation('relu')(stack2)
+    path0 = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(X)
+    path0 = Activation('relu')(path0)
 
     RCL = Conv2D(filters=filedepth, kernel_size=kernel_size, strides=(1, 1), padding='same',
                  kernel_regularizer=keras.regularizers.l2(0.00004),
@@ -46,30 +45,31 @@ def RCL(input, kernel_size, filedepth):
                                                                        distribution='normal',
                                                                        seed=None))
 
-    conv2 = RCL(stack2)
-    stack3 = Add()([conv1, conv2])
-    stack4 = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(stack3)
-    stack4 = Activation('relu')(stack4)
+    conv1 = RCL(path0)
+    stack3 = Add()([X, conv1])
+
+    path1 = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(stack3)
+    path1 = Activation('relu')(path1)
 
     conv3 = Conv2D(filters=filedepth, kernel_size=kernel_size, strides=(1, 1), padding='same',
                    weights=RCL.get_weights(),
                    kernel_regularizer=keras.regularizers.l2(0.00004),
                    kernel_initializer=keras.initializers.VarianceScaling(scale=2.0, mode='fan_in',
-                                                                         distribution='normal', seed=None))(stack4)
+                                                                         distribution='normal', seed=None))(path1)
     stack5 = Add()([conv1, conv3])
-    stack6 = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(stack5)
-    stack6 = Activation('relu')(stack6)
+    path2 = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(stack5)
+    path2 = Activation('relu')(path2)
 
     conv4 = Conv2D(filters=filedepth, kernel_size=kernel_size, strides=(1, 1), padding='same',
                    weights=RCL.get_weights(),
                    kernel_regularizer=keras.regularizers.l2(0.00004),
                    kernel_initializer=keras.initializers.VarianceScaling(scale=2.0, mode='fan_in',
-                                                                         distribution='normal', seed=None))(stack6)
+                                                                         distribution='normal', seed=None))(path2)
     stack7 = Add()([conv1, conv4])
-    stack8 = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(stack7)
-    stack8 = Activation('relu')(stack8)
+    path3 = BatchNormalization(axis=channel_axis, momentum=0.9997, scale=False)(stack7)
+    path3 = Activation('relu')(path3)
 
-    return stack8
+    return path3
 
 
 def IRCNN_block(input):
@@ -78,14 +78,14 @@ def IRCNN_block(input):
     else:
         channel_axis = -1
 
-    branch_0 = RCL(input, (1, 1), 64)
+    branch0 = RCL(input, (1, 1), 64)
 
-    branch_1 = RCL(input, (3, 3), 128)
+    branch1 = RCL(input, (3, 3), 128)
 
-    branch_2 = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(input)
-    branch_2 = RCL(branch_2, (1, 1), 64)
+    branch2 = AveragePooling2D((3, 3), strides=(1, 1), padding='same')(input)
+    branch2 = RCL(branch2, (1, 1), 64)
 
-    x = keras.layers.concatenate([branch_0, branch_1, branch_2], axis=channel_axis)
+    x = keras.layers.concatenate([branch0, branch1, branch2], axis=channel_axis)
     return x
 
 
@@ -97,29 +97,29 @@ def IRRCNN_model(input):
         #     inputShape = (64, 64, 3)
         channel_axis = -1
 
-    net = Conv2D(32, (3, 3), strides=(2, 2), padding='valid')(input)
-    net = conv_bn(net, 32, 3, 3, padding='valid')
-    net = conv_bn(net, 64, 3, 3)
+    model = Conv2D(32, (3, 3), strides=(2, 2), padding='valid')(input)
+    model = conv_bn(model, 32, 3, 3, padding='valid')
+    model = conv_bn(model, 64, 3, 3)
 
-    net = IRRCNN_block(input)
+    model = IRRCNN_block(input)
 
-    net = conv_bn(net, 32, 3, 3, strides=(2, 2), padding='valid')
-    net = MaxPooling2D((3, 3), strides=(2, 2), padding='valid')(net)
-    net = Dropout(0.5)(net)
+    model = conv_bn(model, 32, 3, 3, strides=(2, 2), padding='valid')
+    model = MaxPooling2D((3, 3), strides=(2, 2), padding='valid')(model)
+    model = Dropout(0.5)(model)
 
-    net = IRRCNN_block(input)
+    model = IRRCNN_block(input)
 
-    net = conv_bn(net, 32, 3, 3, strides=(2, 2), padding='valid')
-    net = MaxPooling2D((3, 3), strides=(2, 2), padding='valid')(net)
-    net = Dropout(0.5)(net)
+    model = conv_bn(model, 32, 3, 3, strides=(2, 2), padding='valid')
+    model = MaxPooling2D((3, 3), strides=(2, 2), padding='valid')(model)
+    model = Dropout(0.5)(model)
 
-    net = IRRCNN_block(input)
+    model = IRRCNN_block(input)
 
-    net = conv_bn(net, 32, 3, 3, strides=(2, 2), padding='valid')
-    net = GlobalAveragePooling2D()(net)
-    net = Dropout(0.5)(net)
+    model = conv_bn(model, 32, 3, 3, strides=(2, 2), padding='valid')
+    model = GlobalAveragePooling2D()(model)
+    model = Dropout(0.5)(model)
 
-    return net
+    return model
 
 
 def IRRCNN_block(input):
@@ -133,7 +133,8 @@ def IRRCNN_block(input):
 
     return net
 
-def trainingIRRCNN(input, ep):
+def trainingIRRCNN(image_size, ep):
+    X_train, y_train = preprocessing.loadTrainingSet()
 
     if K.image_data_format() == 'channels_first':
         inputs = keras.layers.Input(shape=(3, image_size, image_size))
@@ -148,6 +149,6 @@ def trainingIRRCNN(input, ep):
     model = keras.models.Model(input, x, name='IRRCNN')
 
     opt = keras.optimizers.SGD(lr=1e-2)
-    model.compile(loss='sparse_categorical_crossentropy', optimizer= opt, metrics=["accuracy"])
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=["accuracy"])
     history = model.fit(x = X_train, y = y_train, epochs=ep, batch_size=32)
     model.save("IRRCNN.h5")
